e494617bbaad4279aa7709e87a7709d189bf4278
Edward McFarlane <edward.mcfarlane@outlook.com>
2016-01-24 01:30:13 +0000

    Simplified

--- main.go
+++ main.go
@@ -13,20 +13,25 @@ type Node struct {
 	value       int
 }
 
-func (root *Node) createTree(rootNodeValue, treeDepth int) {
-	root.value = rootNodeValue
-
+func (root *Node) growTree(treeDepth int) {
 	if treeDepth > 0 {
 		if root.left == nil {
 			root.left = &Node{}
 			root.right = &Node{}
 		}
-		root.left.createTree(2*rootNodeValue-1, treeDepth-1)
-		root.right.createTree(2*rootNodeValue, treeDepth-1)
-
-	} else if root.left != nil {
+		root.left.growTree(treeDepth - 1)
+		root.right.growTree(treeDepth - 1)
+	} else {
 		root.left = nil
 		root.right = nil
+	}
+}
+
+func (root *Node) populateTree(nodeValue int) {
+	root.value = nodeValue
+	if root.left != nil {
+		root.left.populateTree(2*nodeValue - 1)
+		root.right.populateTree(2 * nodeValue)
 	}
 }
 
@@ -50,32 +55,28 @@ func main() {
 		maxDepth = minDepth + 2
 	}
 
+	stretchTree := &Node{}
+	stretchTree.growTree(maxDepth + 1)
+	fmt.Printf("stretch tree of depth %d\t check: %d\n", maxDepth+1, stretchTree.computeTreeChecksum())
+
+	longLivedTree := stretchTree
+	longLivedTree.growTree(maxDepth)
+
 	var wg = &sync.WaitGroup{}
-	buf := make([]string, maxDepth+2)
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		stretchTree := &Node{}
-		stretchTree.createTree(0, maxDepth+1)
-		buf[maxDepth+1] = fmt.Sprintf("stretch tree of depth %d\t check: %d\n", maxDepth+1, stretchTree.computeTreeChecksum())
-	}()
-
-	longLivedTree := &Node{}
-	longLivedTree.createTree(0, maxDepth)
-
+	buf := make([]string, maxDepth+1)
 	for d := minDepth; d <= maxDepth; d += 2 {
 		wg.Add(1)
 		go func(depth, iterations int) {
 			defer wg.Done()
 
 			treeRoot := &Node{}
+			treeRoot.growTree(depth)
+
 			var check int
 			for i := 0; i < iterations; i++ {
-				treeRoot.createTree(i, depth)
+				treeRoot.populateTree(i)
 				check += treeRoot.computeTreeChecksum()
-				treeRoot.createTree(-i, depth)
+				treeRoot.populateTree(-i)
 				check += treeRoot.computeTreeChecksum()
 			}
 			buf[depth] = fmt.Sprintf("%d\t trees of depth %d\t check: %d\n", iterations*2, depth, check)
@@ -83,7 +84,6 @@ func main() {
 	}
 	wg.Wait()
 
-	fmt.Print(buf[maxDepth+1])
 	for depth := minDepth; depth <= maxDepth; depth += 2 {
 		fmt.Print(buf[depth])
 	}
